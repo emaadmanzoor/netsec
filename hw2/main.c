aa782c56db035940e821aa13b4c22d7e42e05782
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2016-03-15 17:03:36 -0400

    initial packet capture

--- hw2/main.c
+++ hw2/main.c
@@ -10,10 +10,18 @@ static const char optstring[] = "i:r:s:";
 static const char usage[] = "usage: %s [-i interface] [-r file] [-s string] expr\n";
 
 int main(int argc, char *argv[]) {
-  char c;
-  char *iname;
-  char *fname;
-  char *mstring;
+  char c; // for getopt
+  char *dev; // interface name, pcap device
+  char *fname; // filename
+  char *mstring; // matching string
+  char *expr; // expression
+  char errbuf[PCAP_ERRBUF_SIZE]; // pcap error buffer
+  pcap_t *handle;
+  struct bpf_program fexp; // compiled filter expression
+  bpf_u_int32 netmask; // netmask of pcap device
+  bpf_u_int32 netip; // ip of snipping device
+  struct pcap_pkthdr header;
+  const u_char *packet;
   bool interface = false;
   bool file = false;
   bool string = false;
@@ -26,7 +34,7 @@ int main(int argc, char *argv[]) {
   while ((c = getopt(argc, argv, optstring)) != -1) {
     switch (c) {
       case 'i':
-        iname = optarg;
+        dev = optarg;
         interface = true;
         break;
       case 'r':
@@ -44,19 +52,53 @@ int main(int argc, char *argv[]) {
     }
   } 
 
+  // cannot use both a interface and a file
   if (interface && file) {
     fprintf(stderr, "Only one of -i or -r should be specified.\n");
     fprintf(stderr, usage, argv[0]);
     exit(-1);
   }
 
-  if (interface)
-    printf("interface: %s\n", iname);
-  if (file)
-    printf("file: %s\n", fname);
-  if (string)
-    printf("string: %s\n", mstring);
-  printf("expression: %s\n", argv[argc-1]);
+  if (interface) { // if an interface was specified
+    handle = pcap_open_live(dev, BUFSIZ, 1, 65536, errbuf);
+  } else if (file) { // if a file was specified
+    handle = pcap_open_offline(fname, errbuf);
+  } else { // if neither an interface nor a file was specified
+    dev = pcap_lookupdev(errbuf);
+    if (dev == NULL) {
+      fprintf(stderr, "Couldn't find default device: (%s)\n", errbuf);
+      exit(-1);
+    }
+    handle = pcap_open_live(dev, BUFSIZ, 1, 65536, errbuf);
+  }
+  
+  // check if handle is valid
+  if (handle == NULL) {
+    fprintf(stderr, "Couldn't open device %s: (%s)\n", dev, errbuf);
+    exit(-1);
+  }
+
+  if (pcap_lookupnet(dev, &netip, &netmask, errbuf) == -1) {
+    fprintf(stderr, "Can't get netmask for device %s\n", dev);
+    netip = 0;
+    netmask = 0;
+  }
+
+  expr = argv[argc-1];
+  printf("Using expression: %s\n", expr);
+  if (pcap_compile(handle, &fexp, expr, 0, netip) == -1) {
+    fprintf(stderr, "Couldn't parse filter %s: (%s)\n", expr, pcap_geterr(handle));
+    exit(-1);
+  }
+
+  if (pcap_setfilter(handle, &fexp) == -1) {
+    fprintf(stderr, "Couldn't install filter %s: (%s)\n", expr, pcap_geterr(handle));
+    exit(-1);
+  }
+
+  packet = pcap_next(handle, &header);
+  printf("Jacked a packet with length of [%d]\n", header.len);
+  pcap_close(handle);
 
   return 0;
 }
