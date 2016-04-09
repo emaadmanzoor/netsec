4be7d3da389638f0996fbfce262e3e94ea934555
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2016-04-08 23:03:04 -0400

    implement client

--- hw3/encrypt.h
+++ hw3/encrypt.h
@@ -3,5 +3,10 @@
  * License: Apache License, Version 2.0
  * https://github.com/emaadmanzoor/netsec
  */
-#define MAX_KEY_LEN 100
-#define BUF_SIZE    10
+#include <openssl/aes.h>
+
+struct ctr_state { 
+  unsigned char ivec[16];
+  unsigned int num;
+  unsigned char ecount[16];
+}; 
--- hw3/main.c
+++ hw3/main.c
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <getopt.h>
 #include <netdb.h>
+#include <openssl/aes.h>
+#include <openssl/rand.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -15,6 +17,7 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 
+#include "encrypt.h"
 #include "param.h"
 
 extern char *optarg;
@@ -27,11 +30,28 @@ int main(int argc, char *argv[]) {
   int inport;
   int outport;
   char *keyfilename;
-  char key[MAX_KEY_LEN];
+  unsigned char key[MAX_KEY_LEN];
   char *dest;
   bool reverse = false;
   FILE *f;
   struct in_addr *addr;
+  char destip[BUF_SIZE];
+
+  // codec
+  int nbytes;
+  char buffer[BUF_SIZE] = {0};
+  char inbuffer[BUF_SIZE+8] = {0};
+  char encryp[BUF_SIZE] = {0};
+  char decryp[BUF_SIZE] = {0};
+  char payload[BUF_SIZE+8];
+  unsigned char iv[8] = {0};
+  struct ctr_state state;
+  AES_KEY aes_key;
+  
+  // socket io
+  struct hostent *server;
+  struct sockaddr_in serv_addr;
+  int sockfd;
 
   while ((c = getopt(argc, argv, optstring)) != -1) {
     switch (c) {
@@ -54,7 +74,7 @@ int main(int argc, char *argv[]) {
 
   fprintf(stderr, "Starting pbproxy: (");
   if (reverse)
-    fprintf(stderr, " In-port=%d ", inport);
+    fprintf(stderr, " Server mode, In-port=%d ", inport);
   fprintf(stderr, " Keyfile=%s ", keyfilename);
   fprintf(stderr, " Dest=%s ", dest);
   fprintf(stderr, " Out-port=%d )\n", outport);
@@ -65,11 +85,135 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Could not open keyfile: %s\n", keyfilename);
     exit(-1);
   }
-  fgets(key, MAX_KEY_LEN, f);
+  fgets((char*) key, MAX_KEY_LEN, f);
 
   // convert destination host to ip address
-  addr = (struct in_addr *) (gethostbyname(dest)->h_addr_list)[0];
-  printf("IP %s\n", inet_ntoa(*addr));
+  server = gethostbyname(dest);
+  addr = (struct in_addr *) (server->h_addr_list)[0];
+  strcpy(destip, inet_ntoa(*addr));
+
+  //fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+
+  if (reverse == true) {
+    // start server    
+  } else {
+    // start client
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    memmove((char *)&serv_addr.sin_addr.s_addr,
+            (char *)server->h_addr, server->h_length);
+    serv_addr.sin_port = htons(outport);
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+      printf("Error connecting to destination socket\n");
+      printf("Start server with: ncat -e /bin/cat -k -l <port>\n");
+      printf("Connect with: ./pbproxy -k mykey localhost <port>\n");
+      exit(-1);
+    }
+
+    AES_set_encrypt_key(key, 128, &aes_key);
+
+    while (true) {
+      // read input into buffer
+      while ((nbytes = read(0, buffer, BUF_SIZE)) > 0) {
+        printf("\tInput bytes: ");
+        for (int i = 0; i < nbytes; i++)
+          printf("%02x ", buffer[i]);
+        printf("\n");
+
+        // initialisation vector bytes
+        RAND_bytes(iv, 8);
+
+        // init ctr
+        state.num = 0;
+        memset(state.ecount, 0, 16);
+        memset(state.ivec + 8, 0, 8);
+        memcpy(state.ivec, iv, 8);
+
+        // encrypt
+        AES_ctr128_encrypt((unsigned char *) buffer,
+                           (unsigned char *) encryp, nbytes,
+                           &aes_key, state.ivec, state.ecount,
+                           &state.num);
+        
+        // setup payload
+        memset(payload, 0, BUF_SIZE + 8);
+        memcpy(payload, iv, 8); // iv is in the first 8 bytes
+        memcpy(payload + 8, encryp, nbytes); // the rest is the data
+
+        write(sockfd, payload, nbytes + 8);
+       
+        printf("\tSent IV (8 bytes): ");
+        for (int i = 0; i < 8; i++)
+          printf("%02x ", (unsigned char) payload[i]);
+        printf("\n");
+        
+        printf("\tSent encrypted data (%d bytes): ", nbytes);
+        for (int i = 0; i < nbytes; i++)
+          printf("%02x ", (unsigned char) payload[i+8]);
+        printf("\n");
+
+        printf("\tSent %d bytes.\n", nbytes + 8);
+      
+        printf("\n");
+
+        // give receiver a chance to run
+        if (nbytes < BUF_SIZE)
+          break;
+
+        /*for (int i = 0; i < nbytes; i++)
+          printf("%02x ", (unsigned char) buffer[i]);
+        printf("\n");
+        
+        for (int i = 0; i < nbytes; i++)
+          printf("%02x ", (unsigned char) encryp[i]);
+        printf("\n");
+
+        for (int i = 0; i < nbytes; i++)
+          printf("%02x ", (unsigned char) decryp[i]);
+        printf("\n");*/
+      }
+
+      // receive data from socket
+      while ((nbytes = read(sockfd, inbuffer, BUF_SIZE+8)) > 0) {
+        printf("\tReceived %d bytes.\n", nbytes);
+        printf("\tIV (8 bytes): ");
+        for (int i = 0; i < 8; i++)
+          printf("%02x ", (unsigned char) inbuffer[i]);
+        printf("\n");
+
+        memcpy(iv, inbuffer, 8); // iv is in first 8 bytes
+        
+        state.num = 0;
+        memset(state.ecount, 0, 16);
+        memset(state.ivec + 8, 0, 8);
+        memcpy(state.ivec, iv, 8);
+
+        memset(decryp, 0, BUF_SIZE);
+
+        AES_ctr128_encrypt((unsigned char *) inbuffer+8,
+                           (unsigned char *) decryp, nbytes-8,
+                           &aes_key, state.ivec, state.ecount,
+                           &state.num);
+
+        printf("\tDecrypted data (%d bytes): ", nbytes - 8);
+        for (int i = 8; i < nbytes; i++)
+          printf("%02x ", (unsigned char) inbuffer[i]);
+        printf("\n");
+
+        printf("\tDecrypted string: ");
+        printf("%.*s\n", nbytes-8, decryp);
+
+        printf("\n");
+
+        // give sender a chance to run
+        if (nbytes < BUF_SIZE + 8)
+          break;
+      }
+    }
+  } 
 
   return 0;
 }
